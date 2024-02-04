c81a284acbc1bd5b1bec814d2a45962bcd8e66c4
ANTARIP SAHA <158764533+Antarip1047@users.noreply.github.com>
2024-02-04 13:21:20 +0000

    datatypes

--- 01_basic/01_dataTypes.js
+++ 01_basic/01_dataTypes.js
@@ -1,1 +1,22 @@
+"use strict";// to use newer version
+// alert(3+3) // not availabke on node js, but available on browser
+
 console.log(3+3);
+console.log("Antarip");
+
+let name = "Antarip"
+let age = 21
+let isLoggedIn =  false
+let state;
+
+// number  range 2 to power 53
+// bigint
+// string = ""
+// boolean true/ false
+// null -> standalone value
+// undefined -> not initialized to assigned yet
+// symbol -> unique
+
+//object
+
+console.log(typeof age);
