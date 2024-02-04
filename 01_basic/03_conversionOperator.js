7bcf92b43d3a061d4b9040a5a79adf66c32f87a3
ANTARIP SAHA <158764533+Antarip1047@users.noreply.github.com>
2024-02-04 13:35:21 +0000

    conversion of data types

---
+++ 01_basic/03_conversionOperator.js
@@ -0,0 +1,23 @@
+let score = "null"
+console.log(typeof score);
+
+let valueInNumber =  Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" -> 33
+// "33abc" -> NaN
+
+let isLoggedIn = "antarip"
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+// 0 false , 1 true
+// "" false
+// "antarip" true
+
+let someNumber = 33
+let stringnum = String(someNumber)
+console.log(stringnum);
+console.log(typeof stringnum);
