bb44831c96e00b1b893daf8ef4755937af38ae83
ANTARIP SAHA <158764533+Antarip1047@users.noreply.github.com>
2024-02-05 19:24:07 +0000

    conversion and comparison

--- 01_basic/03_conversionOperator.js
+++ 01_basic/03_conversionOperator.js
@@ -1,9 +1,9 @@
 let score = "null"
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber =  Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 // "33" -> 33
@@ -11,7 +11,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "antarip"
 let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+//console.log(booleanIsloggedIn);
 
 // 0 false , 1 true
 // "" false
@@ -19,5 +19,37 @@ console.log(booleanIsloggedIn);
 
 let someNumber = 33
 let stringnum = String(someNumber)
-console.log(stringnum);
-console.log(typeof stringnum);
+//console.log(stringnum);
+//console.log(typeof stringnum);
+
+// OPERATIONS
+
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1= "Antarip"
+let str2= " Saha"
+let str3= str1+str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//iif string first then .. all considered as string else other operation present at forst are treated and solved
+
+//console.log(true);
+//console.log(+"");
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter); // read prefix and postfix from mdm
---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,9 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//console.log("2" >  1); // might give not desired output, better to stick to same datatype
+
+console.log(null>=0)
