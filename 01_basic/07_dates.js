b8935c25f5d592e15a734ac5a9ca0b53b5340c26
ANTARIP SAHA <158764533+Antarip1047@users.noreply.github.com>
2024-02-12 18:15:17 +0000

    dates

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+//console.log(myDate.toString());//day,date, time
+//console.log(myDate.toDateString());//day,date
+//console.log(myDate.toLocaleString());//date,time
+//console.log(typeof(myDate));//object
+
+let myCreateDate = new Date(2024,0,6,5,3)
+//console.log(myCreateDate.toDateString());
+
+//console.log(myCreateDate.toLocaleDateString());
+
+let myTimeStamp= Date.now()
+//console.log(myTimeStamp);//in millisecond
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
